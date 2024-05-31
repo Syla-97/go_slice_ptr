c1c025d935fccd5e1dc6681c28fe70c90608fc3f
Syla-97 <rotus7.dev@gmail.com>
2024-05-31 23:42:21 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type ttt struct {
+	str string
+	num	int
+}
+
+func main() {
+	var test []ttt
+	var a, b ttt
+
+	a.str = "aaa"
+	a.num = 1
+
+	b.str = "bbb"
+	b.num = 2
+
+	test = append(test, a)
+	test = append(test, b)
+
+	sub(&test)
+	fmt.Println(test)
+}
+
+func sub(test *[]ttt) {
+	for i := 0; i < len(*test); i++ {
+		fmt.Println((*test)[i].str)
+		fmt.Println((*test)[i].num)
+		if (*test)[i].str == "aaa" {
+			(*test)[i].str = "ccc"
+		}
+	}
+}
